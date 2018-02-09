80c87c41ae6c569795cc162c36a81ef62b3f800c
Hakan Metin <hakan.metin@lip6.fr>
2018-02-09 13:01:31 +0100

    Move solvers into examples add target to makefile

--- makefiles/Examples.mk
+++ makefiles/Examples.mk
@@ -2,9 +2,11 @@
 EXAMPLES := examples/
 
 examples: default $(BIN)CNFBlissSymmetries $(BIN)CNFSaucySymmetries
+solvers: default $(BIN)minisat
 
 $(call REQUIRE-DIR, $(BIN)CNFBlissSymmetries)
 $(call REQUIRE-DIR, $(BIN)CNFSaucySymmetries)
+$(call REQUIRE-DIR, $(BIN)minisat)
 
 $(BIN)CNFBlissSymmetries: LDFLAGS += -lcosy -lbliss  -lz
 $(BIN)CNFBlissSymmetries: $(EXAMPLES)CNFBlissSymmetries.cc
@@ -14,4 +16,17 @@ $(BIN)CNFSaucySymmetries: LDFLAGS += -lcosy -lsaucy  -lz
 $(BIN)CNFSaucySymmetries: $(EXAMPLES)CNFSaucySymmetries.cc
 	$(call cmd-cxx-bin, $@, $<, $(LDFLAGS))
 
-.PHONY: examples
+
+##### Solvers
+# Minisat
+
+FORCE:
+$(BIN)minisat: FORCE
+	$(call cmd-make, , $(EXAMPLES)solvers/minisat/core)
+	$(call cmd-cp, $@, $(EXAMPLES)solvers/minisat/core/minisat)
+
+clean-solvers:
+	$(call cmd-make, clean, $(EXAMPLES)solvers/minisat/core)
+
+
+.PHONY: examples FORCE
