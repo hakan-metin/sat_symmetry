40d222ac74f751b8bd1ec9a9f4fc40e77ec31d22
Hakan Metin <hakan.metin@lip6.fr>
2018-02-13 15:48:48 +0100

    Add SPFS status file

---
+++ src/SPFSStatus.cc
@@ -0,0 +1,79 @@
+// Copyright 2017 Hakan Metin - LIP6
+
+#include "cosy/SPFSStatus.h"
+
+namespace cosy {
+
+
+SPFSStatus::SPFSStatus(const Permutation &permutation,
+                       const Assignment& assignment) :
+    _permutation(permutation),
+    _assignment(assignment),
+    _lookup_index(0),
+    _reasonOfInactive(kNoLiteralIndex),
+    _amountForActive(0) {
+}
+
+SPFSStatus::~SPFSStatus() {
+}
+
+
+void SPFSStatus::updateNotify(const Literal& literal) {
+    DCHECK(!_permutation.isTrivialImage(literal));
+    DCHECK(_assignment.literalIsTrue(literal));
+
+    const Literal image = _permutation.imageOf(literal);
+    const Literal inverse = _permutation.inverseOf(literal);
+
+    _notified.push_back(literal);
+
+    const bool isInactive = _reasonOfInactive != kNoLiteralIndex;
+    if (isInactive)
+        return;
+
+    if (_assignment.isDecision(inverse)) {
+        if (_assignment.literalIsTrue(inverse)) {
+            --_amountForActive;
+        } else {
+            DCHECK(_assignment.literalIsFalse(inverse));
+            _reasonOfInactive = literal.index();
+        }
+    }
+
+    if (_assignment.isDecision(literal)) {
+        if (!_assignment.literalIsAssigned(image)) {
+            ++_amountForActive;
+        } else if (_assignment.literalIsFalse(image)) {
+            _reasonOfInactive = literal.index();
+        }
+    }
+}
+
+void SPFSStatus::updateCancel(const Literal& literal) {
+    DCHECK(!_permutation.isTrivialImage(literal));
+    DCHECK(_assignment.literalIsAssigned(literal));
+    DCHECK(_notified.size() > 0 && _notified.back() == literal);
+
+    const Literal image = _permutation.imageOf(literal);
+    const Literal inverse = _permutation.inverseOf(literal);
+
+    _notified.pop_back();
+    _lookup_index = 0;
+
+
+    const bool isInactive = _reasonOfInactive != kNoLiteralIndex;
+    if (isInactive && _reasonOfInactive != literal.index())
+        return;
+
+    _reasonOfInactive = kNoLiteralIndex;
+
+    const bool literalIsDecision = _assignment.isDecision(literal);
+    if (literalIsDecision && !_assignment.literalIsAssigned(image))
+        --_amountForActive;
+
+    if (_assignment.isDecision(inverse) && _assignment.literalIsTrue(inverse))
+        ++_amountForActive;
+}
+
+
+}  // namespace cosy
